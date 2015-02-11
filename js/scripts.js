ca751882a1155dad7db93f4eabb8752797964125
Clem Freeman and J. Turner <turner.j6@gmail.com>
2015-02-11 10:58:51 -0800

    Add logic and functionality for quarter.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,5 @@
-var coinCombo =
+var coinCombo = function(change) {
+  var quarter = change / 25;
+  var results = quarter.toString().concat(" quarters");
+  return results;
+}
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,11 @@
 describe('coinCombo', function () {
-  it(, function() {
-    expect();
+
+  it('returns the correct amount of quarters in a dollar', function() {
+    expect(coinCombo(100)).to.equal("4 quarters");
   });
+
+  it('returns the correct amount of dimes in 20 cents', function() {
+    expect(coinCombo(20)).to.equal("2 dimes");
+  });
+
 });
