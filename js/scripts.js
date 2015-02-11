9278fd9d84ba170b4115a9941e30b83f812b4b2c
Clem Freeman and J. Turner <clemfree@gmail.com>
2015-02-11 11:10:58 -0800

    Add logic and functionality for dime.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,16 @@
 var coinCombo = function(change) {
-  var quarter = change / 25;
-  var results = quarter.toString().concat(" quarters");
+  var results = "";
+
+  if ( change >= 25 ) {
+    var quarter = change / 25;
+    change = change % 25;
+    results = quarter.toString().concat(" quarters");
+  }
+  if (change >= 10) {
+    var dime = change / 10;
+    change = change % 10;
+    results = results + (dime.toString() + " dimes");
+  }
+
   return results;
 }
