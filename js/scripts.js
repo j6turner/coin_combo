5a2fa74b8e821363977e7de60919207e2bd2ac89
Clem Freeman and J. Turner <clemfree@gmail.com>
2015-02-11 11:42:33 -0800

    Add logic and functionality for nickel; updated functionality to return arrays.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,24 @@
 var coinCombo = function(change) {
   var results = "";
+  var quarter = 0,
+      dime = 0,
+      nickel = 0;
 
   if ( change >= 25 ) {
-    var quarter = change / 25;
+    quarter = change / 25;
     change = change % 25;
-    results = quarter.toString().concat(" quarters");
+
   }
   if (change >= 10) {
-    var dime = change / 10;
+    dime = change / 10;
     change = change % 10;
-    results = results + (dime.toString() + " dimes");
+
+  }
+  if (change >= 5) {
+    nickel = change / 5;
+    change = change % 5;
   }
 
+  results = [quarter, dime, nickel, 0]
   return results;
 }
--- spec/specs.js
+++ spec/specs.js
@@ -1,11 +1,15 @@
 describe('coinCombo', function () {
 
   it('returns the correct amount of quarters in a dollar', function() {
-    expect(coinCombo(100)).to.equal("4 quarters");
+    expect(coinCombo(100)).to.eql([4,0,0,0]);
   });
 
   it('returns the correct amount of dimes in 20 cents', function() {
-    expect(coinCombo(20)).to.equal("2 dimes");
+    expect(coinCombo(20)).to.eql([0,2,0,0]);
   });
 
+  it('returns the least amount of coins for 30 cents', function() {
+    expect(coinCombo(30)).to.eql([1,0,1,0]);
+  });
+  
 });
